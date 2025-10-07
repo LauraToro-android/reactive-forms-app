92fb824a9de02d6fda7befa5a4b5f073e020633a
LauraToro-android <toromonteslaura@gmail.com>
2025-10-07 21:46:08 +0200

    estructura, rutas padre y template HTML, ReactiveForms - FormGroup y FormControl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import authRoutes from './auth/auth.routes';
+import { reactiveRoutes } from './reactive/reactive.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'reactive',
+        loadChildren: () => 
+            import('./reactive/reactive.routes').then((m) =>reactiveRoutes),
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes'),
+    },
+    {
+        path: 'country',
+        loadChildren: () => import('./country/country.routes').then((m) => m.countryRoutes),
+    },
+    {
+        path: '**',
+        redirectTo: 'reactive',
+    },
+];
